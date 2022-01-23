387b81cb18271e8ebe911c74b20bf16f5350aa64
erias04 <github.eliascerne@icloud.com>
2022-01-23 20:54:38 +0100

    create: new page

---
+++ apps/events/pages/events/index.tsx
@@ -0,0 +1,14 @@
+import './index.module.css';
+
+/* eslint-disable-next-line */
+export interface EventsProps {}
+
+export function Events(props: EventsProps) {
+  return (
+    <div>
+      <h1>Welcome to Events!</h1>
+    </div>
+  );
+}
+
+export default Events;
