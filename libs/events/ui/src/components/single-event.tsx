e9120d3ea5a70f85a10ce4dff75cf84666e9ae1b
erias04 <github.eliascerne@icloud.com>
2022-01-23 21:59:21 +0100

    feat: defining props This is the beauty of typescript, to set the expected variable type

--- libs/events/ui/src/components/single-event.tsx
+++ libs/events/ui/src/components/single-event.tsx
@@ -2,9 +2,16 @@ import './single-event.module.css';
 import Link from 'next/link';
 
 /* eslint-disable-next-line */
-export interface SingleEventProps {}
+export interface SingleEventProps {
+  id: string,
+  title: string,
+  image: typeof Image,
+  date: Date,
+  location: Location,
+}
 
 export function SingleEvent(props: SingleEventProps) {
+  const { id, title, image, date, location } = props;
   return (
     <li>
       <img src="" alt="" />
