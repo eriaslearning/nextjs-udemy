a2c72ffd6cdb94789e13f3407b2fae89d8373019
erias04 <github.eliascerne@icloud.com>
2022-01-23 21:55:42 +0100

    feat: added basic html boilerplate

--- libs/events/ui/src/components/single-event.tsx
+++ libs/events/ui/src/components/single-event.tsx
@@ -6,7 +6,18 @@ export interface SingleEventProps {}
 export function SingleEvent(props: SingleEventProps) {
   return (
     <li>
-      
+      <img src="" alt="" />
+      <div>
+        <div>
+          <h2>TITLE</h2>
+        </div>
+        <div>
+          <time>DATE</time>
+        </div>
+        <div>
+          <address>ADDRESS</address>
+        </div>
+      </div>
     </li>
   );
 }
