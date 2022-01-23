e0947c6f8ef525d7631a5cb3ec5ed319a49c0b29
erias04 <github.eliascerne@icloud.com>
2022-01-23 21:07:59 +0100

    feat: rename ...slug component

--- apps/events/pages/events/[...slug].tsx
+++ apps/events/pages/events/[...slug].tsx
@@ -1,12 +1,12 @@
 /* eslint-disable-next-line */
 export interface SlugProps {}
 
-export function Slug(props: SlugProps) {
+export function FilteredEventsPage(props: SlugProps) {
   return (
     <div>
-      <h1>Welcome to Slug!</h1>
+      <h1>Welcome to FilteredEventsPage!</h1>
     </div>
   );
 }
 
-export default Slug;
+export default FilteredEventsPage;
