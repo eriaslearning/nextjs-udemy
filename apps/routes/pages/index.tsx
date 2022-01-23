d8113c97c717cded15121d560687de889b4a4bf0
erias04 <github.eliascerne@icloud.com>
2022-01-23 16:50:08 +0100

    feat: added a Link

--- apps/routes/pages/index.tsx
+++ apps/routes/pages/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 function Index() {
   return (
     <div>
       <h1>Hello World</h1>
+      <Link href="/user">Go to the users page</Link>
     </div>
   )
 }
