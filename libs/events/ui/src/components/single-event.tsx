1bfe4913e10d986897fb2d61d92f71d6b3cb8626
erias04 <github.eliascerne@icloud.com>
2022-01-25 22:24:49 +0100

    feat: added icons

--- libs/events/ui/src/components/single-event.tsx
+++ libs/events/ui/src/components/single-event.tsx
@@ -1,6 +1,10 @@
 import styles from './single-event.module.css';
 import Link from 'next/link';
 
+import DateIcon from './icons/date-icon';
+import ArrowRightIcon from './icons/arrow-right-icon';
+import AddressIcon from './icons/date-icon';
+
 /* eslint-disable-next-line */
 export interface SingleEventProps {
   id: string;
@@ -32,13 +36,22 @@ export function SingleEvent(props: SingleEventProps) {
             <h2>{title}</h2>
           </div>
           <div className={styles['date']}>
+            <DateIcon></DateIcon>
             <time>{humanReadableDate}</time>
           </div>
           <div className={styles['address']}>
+            <AddressIcon></AddressIcon>
             <address>{formattedAddress}</address>
           </div>
           <div className={styles['actions']}>
-            <Link href={exploreLink}>Explore Event</Link>
+            <Link href={exploreLink}>
+              <a>
+                <span>Explore Event</span>
+                <span className={styles['icon']}>
+                  <ArrowRightIcon></ArrowRightIcon>
+                </span>
+              </a>
+            </Link>
           </div>
         </div>
       </li>
