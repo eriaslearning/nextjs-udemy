4f0a8843f76fa3207a3ed6908f690948fe0647f4
erias04 <github.eliascerne@icloud.com>
2022-01-25 22:29:16 +0100

    init: added new library to show a single event

---
+++ libs/event-detail/ui/src/lib/EventDetailUi.tsx
@@ -0,0 +1,14 @@
+import './EventDetailUi.module.css';
+
+/* eslint-disable-next-line */
+export interface EventDetailUiProps {}
+
+export function EventDetailUi(props: EventDetailUiProps) {
+  return (
+    <div>
+      <h1>Welcome to EventDetailUi!</h1>
+    </div>
+  );
+}
+
+export default EventDetailUi;
