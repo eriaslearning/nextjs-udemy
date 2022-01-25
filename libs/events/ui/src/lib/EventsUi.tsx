128c2559006ebf2be0a84f6b753681bcba24b38b
erias04 <github.eliascerne@icloud.com>
2022-01-25 21:17:23 +0100

    feat: added props for displaying Events

--- libs/events/ui/src/lib/EventsUi.tsx
+++ libs/events/ui/src/lib/EventsUi.tsx
@@ -3,14 +3,23 @@ import SingleEvent from '../components/single-event';
 
 /* eslint-disable-next-line */
 export interface EventsUiProps {
-  items?: any,
+  items: any;
 }
 
 export function EventsUi(props: EventsUiProps) {
   const { items } = props;
   return (
     <ul>
-      {items.map((item: any) => <SingleEvent />)}
+      {items.map((item: any) => (
+        <SingleEvent
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          image={item.image}
+          date={item.date}
+          location={item.location}
+        />
+      ))}
     </ul>
   );
 }
