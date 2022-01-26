c06765a82a53f78c2d4d56745202dea6a2a22aae
erias04 <github.eliascerne@icloud.com>
2022-01-26 09:37:45 +0100

    feat: added data for a single event

--- apps/events/pages/events/[eventId].tsx
+++ apps/events/pages/events/[eventId].tsx
@@ -1,11 +1,27 @@
+import { useRouter } from 'next/router';
+import { EventDetailUi } from "@tests/event-detail/ui";
+import { getEventById } from '../../public/dummy-data';
+
 /* eslint-disable-next-line */
 export interface EventIdProps {}
 
 export function EventDetailPage(props: EventIdProps) {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+
+  const eventData = getEventById(eventId);
+  // console.log(eventData);
+
+  // This if statement is also used for letting the computer run a but longer to not get the error of undefined, weird
+  if (!eventData) {
+    return (
+      <h1>404 - Not found</h1>
+    )
+  }
+
+
   return (
-    <div>
-      <h1>Welcome to EventDetailPage!</h1>
-    </div>
+    <EventDetailUi title={eventData.title} image={eventData.image} date={eventData.date} location={eventData.location} text={eventData.description}></EventDetailUi>
   );
 }
 
