6bef24eaf71b29abbaf73c14b61b59f22b971d99
erias04 <github.eliascerne@icloud.com>
2022-01-26 10:35:52 +0100

    feat: added header html

--- libs/layout/ui/src/components/header/header.tsx
+++ libs/layout/ui/src/components/header/header.tsx
@@ -1,13 +1,23 @@
-import './header.module.css';
+import styles from './header.module.css';
+import Link from 'next/link'
 
 /* eslint-disable-next-line */
 export interface HeaderProps {}
 
 export function Header(props: HeaderProps) {
   return (
-    <div>
-      <h1>Welcome to Header!</h1>
-    </div>
+    <header className={styles['header']}>
+      <div className={styles['logo']}>
+        <Link href="/">erias Events</Link>
+      </div>
+      <nav className={styles['navigation']}>
+        <ul>
+          <li>
+            <Link href="/events">All Events</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
