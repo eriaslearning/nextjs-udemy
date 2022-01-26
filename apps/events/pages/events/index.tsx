2cf59bd2c4d52f9aec4ce91a48e90fa1afd11ef9
erias04 <github.eliascerne@icloud.com>
2022-01-26 11:55:08 +0100

    feat: added Search component

--- apps/events/pages/events/index.tsx
+++ apps/events/pages/events/index.tsx
@@ -1,5 +1,6 @@
 import { getAllEvents } from '../../public/dummy-data';
-import { EventsUi } from '@tests/events/ui'
+import { EventsUi } from '@tests/events/ui';
+import { EventsSearch } from '@tests/events/ui';
 
 /* eslint-disable-next-line */
 export interface EventsProps {}
@@ -7,7 +8,10 @@ export interface EventsProps {}
 export function Events(props: EventsProps) {
   const allEvents = getAllEvents();
   return (
-    <EventsUi items={allEvents} />
+    <div>
+      <EventsSearch />
+      <EventsUi items={allEvents} />
+    </div>
   );
 }
 
