d364a99148f1322cdc3f892c97c7f14689bc67c0
erias04 <github.eliascerne@icloud.com>
2022-01-26 23:06:20 +0100

    feat: added logic to fetch a single item and SSR SSR stands for server side rendering

---
+++ apps/ssr/pages/[pid]/index.tsx
@@ -0,0 +1,62 @@
+import fs from 'fs/promises';
+import path from 'path';
+import React, { Fragment } from 'react';
+
+export interface ProductDetailPageProps {
+  loadedProduct: any;
+}
+
+function ProductDetailPage(props: ProductDetailPageProps) {
+  const { loadedProduct } = props;
+  return (
+    <Fragment>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </Fragment>
+  );
+}
+
+async function getData() {
+  const filePath = path.join(
+    process.cwd(),
+    'apps',
+    'ssr',
+    'data',
+    'dummy.json'
+  );
+  const jsonData: any = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return data;
+}
+
+export async function getStaticProps(context: any) {
+  const { params } = context;
+
+  const productId = params.pid;
+  //   console.log(productId);
+
+  const data: any = await getData();
+
+  const product = data.products.find(
+    (product: any) => product.id === productId
+  );
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const data: any = await getData();
+  const ids: any = data.products?.map((product: any) => product.id);
+  const pathsWithParams: any = ids?.map((id: any) => ({ params: { pid: id } }));
+
+  return {
+    paths: pathsWithParams,
+    fallback: false,
+  };
+}
+
+export default ProductDetailPage;
