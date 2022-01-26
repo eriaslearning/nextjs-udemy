44f8cf7ca0b3e2a281363fdafdfcdd19bedb2fb7
erias04 <github.eliascerne@icloud.com>
2022-01-26 10:36:00 +0100

    feat: configured Layout

--- libs/layout/ui/src/lib/LayoutUi.tsx
+++ libs/layout/ui/src/lib/LayoutUi.tsx
@@ -1,13 +1,21 @@
+import { Fragment } from 'react';
+import { ReactNode } from 'react';
 import './LayoutUi.module.css';
+import Header from '../components/header/header'
 
 /* eslint-disable-next-line */
-export interface LayoutUiProps {}
+export interface LayoutUiProps {
+  children: React.ReactNode,
+}
 
 export function LayoutUi(props: LayoutUiProps) {
   return (
-    <div>
-      <h1>Welcome to LayoutUi!</h1>
-    </div>
+    <Fragment>
+      <Header />
+      <main>
+        {props.children}
+      </main>
+    </Fragment>
   );
 }
 
