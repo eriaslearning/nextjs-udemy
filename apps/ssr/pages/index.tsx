b263a305ccb7f66bdce794ae9a7dc8046f5c2a17
erias04 <github.eliascerne@icloud.com>
2022-01-26 17:06:41 +0100

    feat: added error catching

--- apps/ssr/pages/index.tsx
+++ apps/ssr/pages/index.tsx
@@ -1,4 +1,4 @@
-import styles from './index.module.css';
+// import styles from './index.module.css';
 import path from 'path';
 import fs from 'fs/promises';
 
@@ -17,12 +17,23 @@ export function Index(props: IndexProps) {
   );
 }
 
-export async function getStaticProps() {
-  console.log('(Re)-Generating...');
+export async function getStaticProps(context: any) {
+  // console.log(context);
   // cwd is the root directory
   const filePath = path.join(process.cwd(), 'apps', 'ssr', 'data', 'dummy.json');
   const jsonData: any = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data',
+      }
+    }
+  }
+  if (data.products.length === 0) {
+    return { notFound: true }
+  }
 
   return {
     props: {
