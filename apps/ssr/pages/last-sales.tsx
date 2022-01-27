594261886f41e20b717d9a5b9edaf1527089efff
erias04 <github.eliascerne@icloud.com>
2022-01-27 17:53:42 +0100

    feat: added SWR for fetching data

--- apps/ssr/pages/last-sales.tsx
+++ apps/ssr/pages/last-sales.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface LastSalesProps {}
@@ -7,38 +8,78 @@ export interface LastSalesProps {}
 export function LastSales(props: LastSalesProps) {
   const [salesState, setSalesState] = useState(undefined);
   const [isLoading, setIsLoading] = useState(false);
+
+  //   const { data, error } = useSWR(
+  //     'https://nextjs-course-erias-default-rtdb.europe-west1.firebasedatabase.app/sales.json'
+  //   );
+  const { data, error } = useSWR(
+    'https://nextjs-course-erias-default-rtdb.europe-west1.firebasedatabase.app/sales.json',
+    (url) => fetch(url).then((res: any) => res.json())
+  );
+  console.log(data);
+
+  // Rerun every time data updates
   useEffect(() => {
-    setIsLoading(true);
-    fetch(
-      'https://nextjs-course-erias-default-rtdb.europe-west1.firebasedatabase.app/sales.json'
-    )
-      .then((response: any) => response.json())
-      .then((data: any) => {
-        const transformedSales = [];
+    if (data) {
+      const transformedSales = [];
 
-        // Transform from an object into an array
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setSalesState(transformedSales);
-        setIsLoading(false);
-      });
-  }, []);
+      // Transform from an object into an array
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
 
-  if (isLoading) {
-      return <h1>Loading...</h1>
+      setSalesState(transformedSales);
+      console.log(transformedSales)
+    }
+  }, [data]);
+
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     fetch(
+  //       'https://nextjs-course-erias-default-rtdb.europe-west1.firebasedatabase.app/sales.json'
+  //     )
+  //       .then((response: any) => response.json())
+  //       .then((data: any) => {
+  //         const transformedSales = [];
+
+  //         // Transform from an object into an array
+  //         for (const key in data) {
+  //           transformedSales.push({
+  //             id: key,
+  //             username: data[key].username,
+  //             volume: data[key].volume,
+  //           });
+  //         }
+  //         setSalesState(transformedSales);
+  //         setIsLoading(false);
+  //       });
+  //   }, []);
+
+  if (error) {
+    return <h1>Failed to load</h1>;
+  }
+
+  if (!data || !salesState) {
+    return <h1>No data</h1>;
   }
 
   if (!salesState) {
-      return <h1>No data</h1>
+    return <h1>Loading...</h1>;
   }
-  return <ul>
-      {salesState.map((sale: any) => <li key={sale.id}>{sale.username} - ${sale.volume}</li>)}
-  </ul>;
+
+  return (
+    <ul>
+      {salesState.map((sale: any) => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default LastSales;
