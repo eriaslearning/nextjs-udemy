8c9d2fab7fd5eed8465b3ac7895f8a13faba0639
erias04 <github.eliascerne@icloud.com>
2022-01-29 12:45:37 +0100

    feat: added metadata

--- apps/events-v2/pages/index.js
+++ apps/events-v2/pages/index.js
@@ -11,7 +11,10 @@ function HomePage(props) {
     <div>
       <Head>
         <title>Nextjs Events</title>
-        <meta name="description" content="Find a lot of great events that allow you to evolve..." />
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
       </Head>
       <EventList items={props.featuredEvents} />
     </div>
