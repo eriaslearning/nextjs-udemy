c56bb138236a527591fc3d63dc6586252c39629c
erias04 <github.eliascerne@icloud.com>
2022-01-29 19:27:08 +0100

    feat: added a sumbit function

--- apps/api-test/pages/index.tsx
+++ apps/api-test/pages/index.tsx
@@ -1,22 +1,28 @@
+import { useRef } from 'react';
+
 import styles from './index.module.css';
 
 export function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.css file.
-   */
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    
+  }
+
   return (
     <div>
       <h1>erias Form</h1>
-      <form>
+      <form onSubmit={submitFormHandler}>
         <div>
           <label htmlFor="email">Your Email Address</label>
-          <input type="email" id="email" />
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
         <div>
           <label htmlFor="feedback">Your Feedback</label>
-          <textarea id="feedback"></textarea>
+          <textarea id="feedback" rows={5} ref={feedbackInputRef}></textarea>
         </div>
         <button>Send Feedback</button>
       </form>
