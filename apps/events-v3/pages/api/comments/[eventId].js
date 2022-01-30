01bf54f0fc93af4dce754f4ea4ef4ac613f5167c
erias04 <github.eliascerne@icloud.com>
2022-01-30 18:16:46 +0100

    feat: GET, POST to mongodb

--- apps/events-v3/pages/api/comments/[eventId].js
+++ apps/events-v3/pages/api/comments/[eventId].js
@@ -1,5 +1,34 @@
-function handler(req, res) {
+import { MongoClient, ObjectId } from 'mongodb';
+import {
+  connectDatabase,
+  insertDocument,
+  getAllDocuments,
+} from '../../../helpers/db-util';
+
+// async function connectDatabase() {
+//   const client = await MongoClient.connect(
+//     'mongodb+srv://erias89347:LLCtyk00E8hQgRCF@cluster0.f7bqi.mongodb.net/events?retryWrites=true&w=majority'
+//   );
+// }
+
+// async function insertDocument(client, document) {
+//   const db = client.db();
+//   const result = await db.collection('comments').insertOne(document);
+//   return result;
+// }
+
+async function handler(req, res) {
   const eventId = req.query.eventId;
+
+  let client;
+  try {
+    client = await connectDatabase();
+  } catch (err) {
+    res.status(500).json({ message: 'Connexting to the database failed' });
+    return;
+  }
+
+  // const client = await connectDatabase();
 
   if (req.method === 'POST') {
     // add server-side validation
@@ -17,25 +46,37 @@ function handler(req, res) {
     }
 
     const newComment = {
-      id: new Date().toString(),
+      _id: new ObjectId(),
       email,
       name,
       text,
+      eventId,
     };
-    console.log(email, name, text);
+    try {
+      const result = await insertDocument(client, 'comments', newComment);
+      console.log(result);
+    } catch (err) {
+      res.status(500).json({ message: 'Inserting comment failed!' });
+      client.close();
+    }
+
+    // const result = await insertDocument(client, newComment);
+    // console.log(result);
     res
       .status(201)
       .json({ message: 'Added comment successfully', comment: newComment });
   }
 
   if (req.method === 'GET') {
-    const dummyList = [
-      { id: 'c1', name: 'erias', text: 'The first comment' },
-      { id: 'c2', name: 'erias2', text: 'The second comment' },
-    ];
-
-    res.status(200).json({ comments: dummyList });
+    try {
+      const documents = await getAllDocuments(client, 'comments', { _id: -1 });
+      res.status(200).json({ comments: documents });
+    } catch (err) {
+      res.status(500).json({ message: 'Getting comments failed!' });
+    }
   }
+
+  client.close();
 }
 
 export default handler;
--- apps/events-v3/pages/api/newsletter.js
+++ apps/events-v3/pages/api/newsletter.js
@@ -1,4 +1,7 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+import { connectDatabase, insertDocument } from '../../helpers/db-util';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
@@ -6,8 +9,22 @@ function handler(req, res) {
       res.status(422).json({ message: 'Email is invalid' });
       return;
     }
+    let client;
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: 'Connection to the database failed' });
+      return;
+    }
 
-    console.log(userEmail);
+    try {
+      await insertDocument(client, 'newsletter', { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: 'Inserting data failed' });
+      return;
+    }
+
     res.status(201).json({ message: 'Signed up!' });
   } else {
     res.status(400).json({ message: 'Only POST requests are allowed' });
