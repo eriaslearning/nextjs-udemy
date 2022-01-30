56c0987407364cc351a7a9ea8f674aece63ac0de
erias04 <github.eliascerne@icloud.com>
2022-01-30 10:54:44 +0100

    feat: chnaged GET request and added helpers

--- apps/api-test/pages/api/feedback.ts
+++ apps/api-test/pages/api/feedback.ts
@@ -1,12 +1,22 @@
 import fs from 'fs';
 import path from 'path';
 
+function buildFeedbackPath() {
+  return path.join(process.cwd(), 'apps', 'api-test', 'data', 'feedback.json');
+}
+
+function extractFeedback(filePath: any) {
+  const fileData: any = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData);
+  return data;
+}
+
 function handler(req, res) {
   if (req.method === 'POST') {
     const email = req.body.email;
     const feedbackText = req.body.text;
-    
-    const fakeId = (new Date()).toString();
+
+    const fakeId = new Date().toString();
 
     const newFeedback = {
       id: fakeId,
@@ -15,15 +25,15 @@ function handler(req, res) {
     };
 
     // store in a database
-    const filePath = path.join(process.cwd(), 'apps', 'api-test', 'data', 'feedback.json');
-    const fileData: any = fs.readFileSync(filePath);
-
-    const data = JSON.parse(fileData);
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
     data.push(newFeedback);
     fs.writeFileSync(filePath, JSON.stringify(data));
     res.status(201).json({ message: 'Success!', feedback: newFeedback });
   } else {
-    res.status(200).json({ message: 'Hello World' });
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
+    res.status(200).json({ feedback: data });
   }
 }
 
