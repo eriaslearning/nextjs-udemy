14fb94b2d15188c5872ff8571e4e852ec2de501c
erias04 <github.eliascerne@icloud.com>
2022-02-03 23:05:29 +0100

    feat: added onSubmit

--- apps/auth/components/auth/auth-form.js
+++ apps/auth/components/auth/auth-form.js
@@ -34,7 +34,7 @@ function AuthForm() {
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="email">Your Email</label>
           <input type="email" id="email" required ref={emailInputRef} />
