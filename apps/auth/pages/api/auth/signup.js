6459ff53944b838a53198b07e76b29fba0fe09e8
erias04 <github.eliascerne@icloud.com>
2022-02-03 22:18:49 +0100

    feat: added wrong email and password catching

--- apps/auth/pages/api/auth/signup.js
+++ apps/auth/pages/api/auth/signup.js
@@ -4,6 +4,21 @@ async function handler(req, res) {
   const data = req.body;
   const { email, password } = data;
 
+  if (
+    !email ||
+    !email.includes('@') ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res
+      .status(422)
+      .json({
+        message:
+          'Invalid input - The password should also be at least sevent (7) characters long.',
+      });
+    return;
+  }
+
   const client = await connectToDatabase();
 
   const db = client.db();
