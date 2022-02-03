2e34ffb85d4ed1bcf29135d826b17406c19756aa
erias04 <github.eliascerne@icloud.com>
2022-02-03 22:15:29 +0100

    add: signup This file is used to sign the user up

---
+++ apps/auth/pages/api/auth/signup.js
@@ -0,0 +1,14 @@
+import { connectToDatabase } from '../../../lib/db';
+
+async function handler(req, res) {
+  const data = req.body;
+  const { email, password } = data;
+
+  const client = await connectToDatabase();
+
+  const db = client.db();
+
+  db.collection('users');
+}
+
+export default handler;
