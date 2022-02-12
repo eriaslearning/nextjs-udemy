69b27bdad78d1ba3f3337f687b4b6f1d836bfadd
erias04 <github.eliascerne@icloud.com>
2022-02-12 16:03:20 +0100

    feat: added connection to monodb

---
+++ apps/auth/api/auth/signup.js
@@ -0,0 +1,32 @@
+import { connectToDatabase } from '../../lib/db';
+import { hashPassword } from '../../lib/auth';
+
+async function handler(req, res) {
+  const data = req.body;
+
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !email.encludes('@') ||
+    !password ||
+    !password.trim().length < 7
+  ) {
+    res.status(422).json({ message: 'Invalid input!' });
+    return;
+  }
+  const client = await connectToDatabase();
+
+  const db = client.db();
+
+  const hashedPassword = hashPassword(password);
+
+  const result = await db.collection('users').insertOne({
+    email: email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({ message: 'Created user successfully!' });
+}
+
+export default handler;
