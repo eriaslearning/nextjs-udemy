8f76bc21605c9168d0856ebf19b32d982edd4a26
erias04 <github.eliascerne@icloud.com>
2022-02-12 16:03:47 +0100

    feat: changed user credentioals

--- apps/auth/lib/db.js
+++ apps/auth/lib/db.js
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb';
 export async function connectToDatabase() {
   // Need to add a try catch block to handle with errors
   const client = await MongoClient.connect(
-    'mongodb+srv://erias89347:cmt2O5JFU2nfmS08@cluster0.f7bqi.mongodb.net/auth?retryWrites=true&w=majority'
+    'mongodb+srv://erias87983:XQWQAZjpZvPsZbcd@cluster0.f7bqi.mongodb.net/auth?retryWrites=true&w=majority'
   );
   return client;
 }
